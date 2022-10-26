7740023525460837783372199aa8c34fbfd3d0ac
marco-vignozzi <marcovignozzi7@gmail.com>
2022-10-26 15:20:33 +0200

    Added documentation files .tex and .pdf

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from res import test
 # the class Test performs tests about research and insertion speed both with red-black trees and the binary ones
 
 # we can choose to execute the tests with random values or with sequential values with this initialization:
-#                                                                            test_type, start, max value and step
+#                                                                   test_type, research_type, start, max value and step
 
 # NOTE:  more than 900 max value with normal mode won't work, because it can't execute more than 900 recursions in the
 #        binary tree
